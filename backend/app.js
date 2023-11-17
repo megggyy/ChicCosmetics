05bf0b2b2aae5b75952f8a97a47dad74572b39ef
megggyy <shanaimeg.honrado@tup.edu.ph>
2023-11-17 14:40:16 +0800

    brand routes backend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const products = require('./routes/product');
 const auth = require('./routes/auth')
 const order = require('./routes/order')
+const brand= require('./routes/brand');
 
 app.use(cors())
 app.use(express.json({limit:'50mb'}));
@@ -13,6 +14,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cookie());
 app.use('/api/v1', products);
+app.use('/api/v1', brand);
 app.use('/api/v1', auth);
 app.use('/api/v1', order);
 
