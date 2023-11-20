fe9b7a6df1a66f4b0c4f13379c30248b492282de
princessjoyy <princessjpy.romero@tup.edu.ph>
2023-11-20 16:03:48 +0800

    category route

---
+++ backend/routes/category.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../utils/multer');
+
+const { newCategory, updateCategory, getAdminCategories, deleteCategory, getCategoryDetails } = require('../controllers/categoryController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+router.post('/admin/category/new', isAuthenticatedUser, upload.array('images', 10), newCategory);
+router.get('/admin/categories', isAuthenticatedUser, authorizeRoles('admin'),getAdminCategories);
+router.route('/admin/category/:id').get(isAuthenticatedUser, getCategoryDetails ).put(upload.array('images', 10), updateCategory).delete(deleteCategory);
+module.exports = router;
