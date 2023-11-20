659815da7a0ffbe30fbbe156cc03d65531886dd0
princessjoyy <princessjpy.romero@tup.edu.ph>
2023-11-20 16:13:51 +0800

    add categories to product

--- frontend/src/Components/Admin/UpdateProduct.js
+++ frontend/src/Components/Admin/UpdateProduct.js
@@ -13,7 +13,6 @@ const UpdateProduct = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState(0);
     const [description, setDescription] = useState('');
-    const [category, setCategory] = useState('');
     const [stock, setStock] = useState(0);
     const [seller, setSeller] = useState('');
     const [images, setImages] = useState([]);
@@ -28,20 +27,9 @@ const UpdateProduct = () => {
     const [brand, setBrand] = useState(''); // New state for brand
     const [brands, setBrands] = useState([]); // State to store brands
 
-    const categories = [
-        'Electronics',
-        'Cameras',
-        'Laptops',
-        'Accessories',
-        'Headphones',
-        'Food',
-        "Books",
-        'Clothes/Shoes',
-        'Beauty/Health',
-        'Sports',
-        'Outdoor',
-        'Home'
-    ]
+    const [category, setCategory] = useState('');
+    const [categories, setCategories] = useState([]);
+
     let { id } = useParams();
     let navigate = useNavigate();
 
@@ -99,15 +87,32 @@ const UpdateProduct = () => {
         }
     }
 
+    const getAdminCategories = async () => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${getToken()}`,
+                },
+            };
+
+            const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/admin/categories`, config);
+            setCategories(data.categories);
+            setLoading(false);
+        } catch (error) {
+            setError(error.response.data.message);
+        }
+    }
+
     useEffect(() => {
         getAdminBrands();
+        getAdminCategories();
         if (product && product._id !== id) {
             getProductDetails(id)
         } else {
             setName(product.name);
             setPrice(product.price);
             setDescription(product.description);
-            setCategory(product.category);
             setSeller(product.seller);
             setStock(product.stock)
             setOldImages(product.images)
@@ -133,10 +138,10 @@ const UpdateProduct = () => {
         formData.set('name', name);
         formData.set('price', price);
         formData.set('description', description);
-        formData.set('category', category);
         formData.set('stock', stock);
         // formData.set('seller', seller);
         formData.set('brand', brand); // Include brand in the form data
+        formData.set('category', category);
         images.forEach(image => {
             formData.append('images', image)
         })
@@ -195,14 +200,6 @@ const UpdateProduct = () => {
                                     <textarea className="form-control" id="description_field" rows="8" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="category_field">Category</label>
-                                    <select className="form-control" id="category_field" value={category} onChange={(e) => setCategory(e.target.value)}>
-                                        {categories.map(category => (
-                                            <option key={category} value={category} >{category}</option>
-                                        ))}
-                                    </select>
-                                </div>
-                                <div className="form-group">
                                     <label htmlFor="stock_field">Stock</label>
                                     <input
                                         type="number"
@@ -236,6 +233,24 @@ const UpdateProduct = () => {
                                         {brands.map((brand) => (
                                             <option key={brand._id} value={brand._id}>
                                                 {brand.name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="category_field">Category</label>
+                                    <select
+                                        className="form-control"
+                                        id="category_field"
+                                        value={category}
+                                        onChange={(e) => setCategory(e.target.value)}
+                                    >
+                                        <option value="" disabled>
+                                            Select a category
+                                        </option>
+                                        {categories.map((category) => (
+                                            <option key={category._id} value={category._id}>
+                                                {category.name}
                                             </option>
                                         ))}
                                     </select>
