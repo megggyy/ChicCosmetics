7ab8dcad550abf460796938d82893b65d2085ca0
megggyy <shanaimeg.honrado@tup.edu.ph>
2023-11-21 22:11:57 +0800

    dashboard links

--- frontend/src/Components/Admin/Dashboard.js
+++ frontend/src/Components/Admin/Dashboard.js
@@ -46,10 +46,42 @@ const Dashboard = () => {
         }
     }
 
+    const allOrders = async () => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${getToken()}`
+                }
+            };
+
+            const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/admin/orders`, config);
+            setOrders(data.orders);
+        } catch (error) {
+            setError(error.response.data.message);
+        }
+    };
+    
+    const allUsers = async () => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${getToken()}`
+                }
+            };
+
+            const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/admin/users`, config);
+            setUsers(data.users);
+        } catch (error) {
+            setError(error.response.data.message);
+        }
+    };
+
     useEffect(() => {
         getAdminProducts()
-        // allOrders()
-        // allUsers()
+        allOrders()
+        allUsers()
     }, [])
 
     return (
@@ -80,7 +112,7 @@ const Dashboard = () => {
                             </div>
                             <div className="row pr-4">
                                 <div className="col-xl-3 col-sm-6 mb-3">
-                                    <div className="card text-white bg-success o-hidden h-100">
+                                <div style={{ backgroundColor: '#B036FF' }} className="card text-white o-hidden h-100">
                                         <div className="card-body">
                                             <div className="text-center card-font-size">Products<br /> <b>{products && products.length}</b></div>
                                         </div>
@@ -94,11 +126,11 @@ const Dashboard = () => {
                                     </div>
                                 </div>
                                 <div className="col-xl-3 col-sm-6 mb-3">
-                                    <div className="card text-white bg-danger o-hidden h-100">
+                                    <div style={{ backgroundColor: '#FF36E9' }} className="card text-white o-hidden h-100">
 
-                                        {/* <div className="card-body">
+                                        <div className="card-body">
                                             <div className="text-center card-font-size">Orders<br /> <b>{orders && orders.length}</b></div>
-                                        </div> */}
+                                        </div> 
 
                                         <Link className="card-footer text-white clearfix small z-1" to="/admin/orders">
                                             <span className="float-left">View Details</span>
@@ -111,11 +143,11 @@ const Dashboard = () => {
 
 
                                 <div className="col-xl-3 col-sm-6 mb-3">
-                                    <div className="card text-white bg-info o-hidden h-100">
+                                    <div style={{ backgroundColor: '#FF3685' }} className="card text-white o-hidden h-100">
 
-                                        {/* <div className="card-body">
+                                        <div className="card-body">
                                             <div className="text-center card-font-size">Users<br /> <b>{users && users.length}</b></div>
-                                        </div> */}
+                                        </div>
 
                                         <Link className="card-footer text-white clearfix small z-1" to="/admin/users">
                                             <span className="float-left">View Details</span>
@@ -126,9 +158,21 @@ const Dashboard = () => {
                                     </div>
                                 </div>
                                 <div className="col-xl-3 col-sm-6 mb-3">
-                                    <div className="card text-white bg-warning o-hidden h-100">
+                                    <div style={{ backgroundColor: '#FF4B36' }} className="card text-white o-hidden h-100">
                                         <div className="card-body">
                                             <div className="text-center card-font-size">Out of Stock<br /> <b>{outOfStock}</b></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <h1 className="my-4">CHARTS</h1>
+                            <div className="row pr-4">
+                                <div className="col-xl-12 col-sm-12 mb-3">
+                                    <div className="card text-white bg-primary o-hidden h-100">
+                                        <div className="card-body">
+                                            {/* <div className="text-center card-font-size">Total Amount<br /> <b>${totalAmount && totalAmount.toFixed(2)}</b>
+                                            </div> */}
+
                                         </div>
                                     </div>
                                 </div>
