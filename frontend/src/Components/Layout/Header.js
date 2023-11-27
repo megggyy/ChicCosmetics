660e3be84e9fe230b7315693e2e239e490b3327e
megggyy <shanaimeg.honrado@tup.edu.ph>
2023-11-27 15:15:05 +0800

    update user image header

--- frontend/src/Components/Layout/Header.js
+++ frontend/src/Components/Layout/Header.js
@@ -64,11 +64,11 @@ const Header = ({ cartItems }) => {
                     {user ? (<div className="ml-4 dropdown d-inline">
                         <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button" id="dropDownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <figure className="avatar avatar-nav">
-                                <img
-                                    src={user.avatar && user.avatar.url}
-                                    alt={user && user.name}
-                                    className="rounded-circle"
-                                />
+                            <img
+                            src={user.avatar && user.avatar.length > 0 && user.avatar[0].url}
+                            alt={user && user.name}
+                            className="rounded-circle"
+                            />
                             </figure>
                             <span>{user && user.name}</span>
                         </Link>
