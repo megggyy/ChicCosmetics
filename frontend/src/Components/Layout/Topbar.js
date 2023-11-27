e557eb5568cd6f5f4f24cd1167b7a8138efc56ec
megggyy <shanaimeg.honrado@tup.edu.ph>
2023-11-28 01:22:12 +0800

    layout update

--- frontend/src/Components/Layout/Topbar.js
+++ frontend/src/Components/Layout/Topbar.js
@@ -106,27 +106,28 @@ function Topbar() {
                     className="collapse show navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0"
                     id="navbar-vertical"
                   >
-                    <div className="mt-5">
-                      <ul className="pl-0">
+                    <div className="navbar-nav w-100 overflow-hidden" style={{height: '410px'}}>
+                    {/* <div className="nav-item dropdown"> */}
                         {categories.map((category) => (
-                          <li
+                          <Link
+                          className="nav-item nav-link"
                             key={category._id}
-                            style={{
-                              cursor: "pointer",
-                              listStyleType: "none`",
-                              textAlign: "left",
-                              padding: "5px 0",
-                              color:
-                                category._id === selectedCategory
-                                  ? "#8b4656"
-                                  : "black",
-                            }}
+                            // style={{
+                            //   cursor: "pointer",
+                            //   listStyleType: "none`",
+                            //   textAlign: "left",
+                            //   padding: "5px 0",
+                            //   color:
+                            //     category._id === selectedCategory
+                            //       ? "#8b4656"
+                            //       : "black",
+                            // }}
                             onClick={() => selectCategory(category._id)} // Set category filter
                           >
                             {category.name}
-                          </li>
+                          </Link>
                         ))}
-                      </ul>
+                      {/* </div> */}
                     </div>
                   </nav>
                 </div>
@@ -153,137 +154,71 @@ function Topbar() {
                         >
                           <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div
-                          className="collapse navbar-collapse justify-content-between"
-                          id="navbarCollapse"
-                        >
-                          <div className="navbar-nav mr-auto py-0">
-                            <a
-                              href="index.html"
-                              className="nav-item nav-link active"
-                            >
-                              Home
-                            </a>
-                            <a href="shop.html" className="nav-item nav-link">
-                              Shop
-                            </a>
-                            <a href="detail.html" className="nav-item nav-link">
-                              Shop Detail
-                            </a>
-                            <div className="nav-item dropdown">
-                              <a
-                                href="#"
-                                className="nav-link dropdown"
-                                data-toggle="dropdown"
-                              >
-                                Pages ↓
-                              </a>
-                              <div className="dropdown-menu rounded-0 m-0">
-                                <a href="cart.html" className="dropdown-item">
-                                  Shopping Cart
-                                </a>
-                                <a
-                                  href="checkout.html"
-                                  className="dropdown-item"
-                                >
-                                  Checkout
-                                </a>
-                              </div>
-                            </div>
-                            <a
-                              href="contact.html"
-                              className="nav-item nav-link"
-                            >
-                              Contact
-                            </a>
-                          </div>
-                          
-                        </div>
+                        <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div className="navbar-nav mr-auto py-0">
+                        <a href="index.html" className="nav-item nav-link active">Home</a>
+                            <Link to="/shop" className="nav-item nav-link">
+                                Shop
+                            </Link>
+                            <Link to="/wishlist" className="nav-item nav-link">
+                                Wishlist
+                            </Link>
+                            <Link to="/cart" className="nav-item nav-link">
+                                Cart
+                            </Link>
+                    </div>
+                </div>
                       </nav>
 
-                      <div
-                        id="header-carousel"
-                        className="carousel slide"
-                        data-ride="carousel"
-                      >
-                        <div className="carousel-inner">
-                          <div
-                            className="carousel-item active"
-                            style={{ height: "410px" }}
-                          >
-                            <img
-                              className="img-fluid"
-                              src="userkit/img/carousel-1.jpg"
-                              alt="Image"
-                            />
-                            <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                              <div
-                                className="p-3"
-                                style={{ maxWidth: "700px" }}
-                              >
-                                <h4 className="text-light text-uppercase font-weight-medium mb-3">
-                                  10% Off Your First Order
-                                </h4>
-                                <h3 className="display-4 text-white font-weight-semi-bold mb-4">
-                                  Flawless Makeup
-                                </h3>
-                                <a href="" className="btn btn-light py-2 px-3">
-                                  Shop Now
-                                </a>
-                              </div>
+                      <div id="header-carousel" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active" style={{height: '410px'}}>
+                        <img className="img-fluid" src="userkit/img/maybellinebanner.jpg" alt="Image"/>
+                        <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div className="p-3" style={{maxWidth: '700px'}}>
+                                <h4 className="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
+                                <h3 className="display-4 text-white font-weight-semi-bold mb-4">Fashionable Dress</h3>
+                                <Link to="/shop" className="btn btn-light py-2 px-3">
+                                Shop Now
+                                </Link>
                             </div>
-                          </div>
-                          <div
-                            className="carousel-item"
-                            style={{ height: "410px" }}
-                          >
-                            <img
-                              className="img-fluid"
-                              src="userkit/img/carousel-2.jpg"
-                              alt="Image"
-                            />
-                            <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                              <div
-                                className="p-3"
-                                style={{ maxWidth: "700px" }}
-                              >
-                                <h4 className="text-light text-uppercase font-weight-medium mb-3">
-                                  10% Off Your First Order
-                                </h4>
-                                <h3 className="display-4 text-white font-weight-semi-bold mb-4">
-                                  Reasonable Price
-                                </h3>
-                                <a href="" className="btn btn-light py-2 px-3">
-                                  Shop Now
-                                </a>
-                              </div>
-                            </div>
-                          </div>
                         </div>
-                        <a
-                          className="carousel-control-prev"
-                          href="#header-carousel"
-                          data-slide="prev"
-                        >
-                          <div
-                            className="btn btn-dark"
-                            style={{ width: "45px", height: "45px" }}
-                          >
-                            <span className="carousel-control-prev-icon mb-n2"></span>
-                          </div>
-                        </a>
-                        <a
-                          className="carousel-control-next"
-                          href="#header-carousel"
-                          data-slide="next"
-                        >
-                          <div
-                            className="btn btn-dark"
-                            style={{ width: "45px", height: "45px" }}
-                          >
-                            <span className="carousel-control-next-icon mb-n2"></span>
-                          </div>
-                        </a>
+                    </div>
+                    <div className="carousel-item" style={{height: '410px'}}>
+                        <img className="img-fluid" src="userkit/img/nars.jpg" alt="Image"/>
+                        <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div className="p-3" style={{maxWidth: '700px'}}>
+                                <h4 className="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
+                                <h3 className="display-4 text-white font-weight-semi-bold mb-4">Reasonable Price</h3>
+                                <Link to="/shop" className="btn btn-light py-2 px-3">
+                                Shop Now
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item" style={{height: '410px'}}>
+                        <img className="img-fluid" src="userkit/img/benefit.jpg" alt="Image"/>
+                        <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div className="p-3" style={{maxWidth: '700px'}}>
+                                <h4 className="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
+                                <h3 className="display-4 text-white font-weight-semi-bold mb-4">Reasonable Price</h3>
+                                <Link to="/shop" className="btn btn-light py-2 px-3">
+                                Shop Now
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#header-carousel" data-slide="prev">
+                    <div className="btn btn-dark" style={{width: '45px', height: '45px'}}>
+                        <span className="carousel-control-prev-icon mb-n2"></span>
+                    </div>
+                </a>
+                <a className="carousel-control-next" href="#header-carousel" data-slide="next">
+                    <div className="btn btn-dark" style={{width: '45px', height: '45px'}}>
+                        <span className="carousel-control-next-icon mb-n2"></span>
+                    </div>
+                </a>
                       </div>
                     </div>
 
@@ -317,6 +252,7 @@ function Topbar() {
                         </div>
                       )}
                     </div>
+
                   </Fragment>
                 ) : (
                   <div className="col-lg-9">
@@ -407,6 +343,7 @@ function Topbar() {
                 </a>
                     </div>
                   </div>
+                  
                 )}
               </div>
             </div>
